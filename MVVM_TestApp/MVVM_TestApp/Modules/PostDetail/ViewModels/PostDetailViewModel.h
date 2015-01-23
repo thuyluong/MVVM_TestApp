0d892e9641a4e922966a85036afe9388bc207827
ThuyLuong <thuyluong2109@gmail.com>
2015-01-23 16:12:53 +0700

    Create Post detail class

---
+++ MVVM_TestApp/MVVM_TestApp/Modules/PostDetail/ViewModels/PostDetailViewModel.h
@@ -0,0 +1,13 @@
+//
+//  PostDetailViewModel.h
+//  MVVM_TestApp
+//
+//  Created by ThuyLuong on 1/23/15.
+//  Copyright (c) 2015 ThuyLuong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PostDetailViewModel : NSObject
+
+@end
---
+++ MVVM_TestApp/MVVM_TestApp/Modules/PostDetail/ViewModels/PostDetailViewModel.m
@@ -0,0 +1,13 @@
+//
+//  PostDetailViewModel.m
+//  MVVM_TestApp
+//
+//  Created by ThuyLuong on 1/23/15.
+//  Copyright (c) 2015 ThuyLuong. All rights reserved.
+//
+
+#import "PostDetailViewModel.h"
+
+@implementation PostDetailViewModel
+
+@end
